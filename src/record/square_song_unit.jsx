210dab312698661bbbed2abce227cc4461770122
QuincyHsu1006 <113991451+QuincyHsu1006@users.noreply.github.com>
2025-08-21 21:03:26 +0800

    square song unit css

--- src/record/square_song_unit.jsx
+++ src/record/square_song_unit.jsx
@@ -1,10 +1,14 @@
 import { DifficultyColors } from "../common/songs"
+import "./square_song_unit.css"
 
 function SquareSongUnit({song}){
     return (
         <>
-            <div className="box" style={{'--bg-color': DifficultyColors[song.difficulty]}}>
-
+            <div className="square" style={{'--bg-color': DifficultyColors[song.difficulty]}}>
+                <div className="img_border">
+                    <img className="song_img" src={song.imgURL}/>
+                    <div className="song_title wd">{song.title}</div>
+                </div>
             </div>
         </>
     )
